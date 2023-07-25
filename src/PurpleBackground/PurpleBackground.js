5be7d7cafc8272f7b7ac73dd022cb4301e0680f2
abelmuro <abelmuro93@gmail.com>
2023-07-24 17:37:40 -0700

    commit

--- src/PurpleBackground/PurpleBackground.js
+++ src/PurpleBackground/PurpleBackground.js
@@ -5,9 +5,6 @@ import brand from './images/credit-card-brand.png';
 function PurpleBackground() {
     return(
         <div className={styles.container}>
-            <div className={styles.oval}></div>
-            <div className={styles.oval}></div>
-            <div className={styles.oval}></div>
             <div className={styles.creditCardFront}>
                 <img className={styles.brand} src={brand}/>
                 <p className={styles.cardNumber}>
