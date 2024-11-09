524dcfa95ca0bdc8c446fe4118feb62b9a510c87
abelmuro <abelmuro93@gmail.com>
2024-11-09 13:37:22 -0800

    commit

--- src/WhiteBackground/Form/Form.js
+++ src/WhiteBackground/Form/Form.js
@@ -4,68 +4,21 @@ import InputName from './InputName'
 import InputCardNumber from './InputCardNumber';
 import AdditionalCardDetails from './AdditionalCardDetails';
 import FormSubmitted from './FormSubmitted';
-import { CardElement, useStripe, useElements} from '@stripe/react-stripe-js'
 
 function Form() {
     const [submitted, setSubmitted] = useState(false);
-    const stripe = useStripe();
-    const elements = useElements();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
-            const response = await fetch('/create_payment_intent', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({amount: 150})
-            })
-
-            if(response.status === 200){
-                const clientSecret = await response.text();
-                /* 
-                
-                const { error, paymentIntent } = await stripe.confirmCardPayment(clientSecret, { 
-                    payment_method: { 
-                        card: elements.getElement(CardElement) 
-                    } }); 
-                    
-                    if (error) { setError(error.message); } 
-                    
-                    else { 
-                        setPaymentIntentId(paymentIntent.id); //payment has been processed
-                    
-                    }
-                
-                */
-            }
-            
-        }
-        catch(error){
-            if(error.type === 'StripeCardError')
-                console.log(error.message);
-            else if(error.type === 'StripeRateLimitError')
-                console.log(error.message);
-            else if(error.type === 'StripeInvalidRequestError')
-                console.log(error.message);
-            else if(error.type === 'StripeAPIError')
-                console.log(error.message);
-            else if(error.type === 'StripeConnectionError')
-                console.log(error.message);
-            else if(error.type === 'StripeAuthenticationError')
-                console.log(error.message);
-            else
-                console.log(error.message);
-        }
         setSubmitted(true);
     }
 
     return submitted ? <FormSubmitted setSubmitted={setSubmitted}/> :
             <form className={styles.container} onSubmit={handleSubmit}>
-                <CardElement/>
                 <InputName/>
                 <InputCardNumber/>
                 <AdditionalCardDetails/>
-                <input type='submit' value='Confirm' className={styles.confirm} disabled={!stripe}/>
+                <input type='submit' value='Confirm' className={styles.confirm}/>
             </form>         
 }
 
--- src/WhiteBackground/WhiteBackground.js
+++ src/WhiteBackground/WhiteBackground.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import styles from './styles.module.css';
 import Form from './Form'
-import { loadStripe } from '@stripe/stripe-js'; 
-import { Elements } from '@stripe/react-stripe-js'
-
-const stripePromise = loadStripe('pk_live_51QIga7EVvi3aeXM6CRxU0gULOfE7SuAGy72ZZUczqkwM3WAtggy3L4K8JQLBFbqloAem8ZeCzcDV5kSDC3I3dGua00hT3pEUjT');
 
 function WhiteBackground() {
     return(
-        <Elements stripe={stripePromise}>
-`            <div className={styles.container}>
-                <Form/>
-            </div>`            
-        </Elements>
-
+        <div className={styles.container}>
+            <Form/>
+        </div>            
     )
 }
 
