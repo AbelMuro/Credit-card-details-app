44524d7a48d5539109e22997ba34fc63573e68f3
abelmuro <abelmuro93@gmail.com>
2024-11-09 19:04:59 -0800

    commit

--- src/WhiteBackground/WhiteBackground.js
+++ src/WhiteBackground/WhiteBackground.js
@@ -4,7 +4,7 @@ import Form from './Form'
 import { loadStripe } from '@stripe/stripe-js'; 
 import { Elements } from '@stripe/react-stripe-js'
 
-const stripePromise = loadStripe('pk_live_51QIga7EVvi3aeXM6CRxU0gULOfE7SuAGy72ZZUczqkwM3WAtggy3L4K8JQLBFbqloAem8ZeCzcDV5kSDC3I3dGua00hT3pEUjT');
+const stripePromise = loadStripe(process.env.secretKey);
 
 const options = {
     fonts: [{family: 'SpaceGrotesk', src: 'url(https://db.onlinewebfonts.com/t/7f510d38d1c785c851d73882c0ca58c0.ttf)', style: 'normal', weight: "500", size: '18px'}]
